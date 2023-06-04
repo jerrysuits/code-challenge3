6c8312ff7912c5a91041e1c630064c814db803dd
Gerald <adudagerald@gmail.com>
2023-06-04 22:24:25 +0300

    finished class review

--- files/review.rb
+++ files/review.rb
@@ -1,3 +1,9 @@
 require 'active_record'
 class Review < ActiveRecord::Base
+     belongs_to :user
+     belongs_to :product
+     
+     def print_review
+       puts "Review for #{product.name} by #{user.name}: #{star_rating}. #{comment}"
+     end
 end
